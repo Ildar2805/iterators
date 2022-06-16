028d36f9c5f94f15bd77b1ef9d0f89536b0792a2
Ildar2805 <ildar2805@gmail.com>
2022-06-16 09:32:10 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,41 @@
+
+
+class FlatIterator:
+
+    def __init__(self, nested_list):
+        self.result = []
+        self.get_result(nested_list)
+
+    def get_result(self, lists):
+        for item in lists:
+            if isinstance(item, list):
+                self.get_result(item)
+            else:
+                self.result.append(item)
+
+    def __iter__(self):
+        self.cursor = -1
+        return self
+
+    def __next__(self):
+        self.cursor += 1
+        if self.cursor == len(self.result):
+            raise StopIteration
+
+        return self.result[self.cursor]
+
+def main():
+    nested_list = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for item in FlatIterator(nested_list):
+        print(item)
+
+    flat_list = [item for item in FlatIterator(nested_list)]
+    print(flat_list)
+
+if __name__ == '__main__':
+    main()
